5fa84946fd45f3f9d77f4f625eeb4fa2816e2076
Kar-theCoder <vchimz.vc@gmail.com>
2023-09-08 01:15:03 +0200

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 /**
- * main - Entry Point
+ * main - A programme that prints text and a new line
  *
- * Return: Always 0 at sucess
+ * Return: Always 0 (Sucess)
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
