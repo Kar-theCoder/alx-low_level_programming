99e7aa7748b80f75ed3a6ec59346ed671d33ada8
Kar-theCoder <vchimz.vc@gmail.com>
2023-09-19 21:10:40 +0200

    Don't hate the hacker, hate the code

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -11,23 +11,23 @@
 
 int main(void)
 {
-	int password[100];
-	int a, summ, num;
+	int passwd[100];
+	int a, sum, num;
 
-	summ = 0;
+	sum = 0;
 
 	srand(time(NULL));
 
 	for ( a = 0; a < 100; a++)
 	{
-		password[a] = rand() % 78;
-		summ += (password[a] + '0');
-		putchar(password[a] + '0');
+		passwd[a] = rand() % 78;
+		sum += (passwd[a] + '0');
+		putchar(passwd[a] + '0');
 
 		if ((2772 - sum) - '0' < 78)
 		{
-			num = 2772 - summ - '0';
-			summ += num;
+			num = 2772 - sum - '0';
+			sum += num;
 			putchar(n + '0');
 			break;
 		}
